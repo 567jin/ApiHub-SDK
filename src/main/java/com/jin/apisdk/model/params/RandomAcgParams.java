c157a75e2b327f03c85abfccca69d62b2a55d9bf
567jin <2737917147@qq.com>
2024-03-07 09:57:14 +0800

    feat: 添加新接口

---
+++ src/main/java/com/jin/apisdk/model/params/RandomAcgParams.java
@@ -0,0 +1,17 @@
+package com.jin.apisdk.model.params;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @author jin
+ * @Description: 随机狗狗
+ */
+@Data
+@Accessors(chain = true)
+public class RandomAcgParams implements Serializable {
+    private static final long serialVersionUID = 3815188540334269470L;
+
+}
